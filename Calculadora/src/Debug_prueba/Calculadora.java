aa58a6a746a76f5a91619685658e5744679ac238
roberto <roberto@localhost>
2023-04-10 17:54:46 +0200

    Segundo commit damA

--- Calculadora/src/Debug_prueba/Calculadora.java
+++ Calculadora/src/Debug_prueba/Calculadora.java
@@ -24,7 +24,7 @@ public class Calculadora {
     }
     
     public int divide(int num1,int num2){
-        if (num2==0) throw new ArithmeticException("NO PUEDES DIVIDIR POR CERO, MELON");
+        if (num2==0) throw new ArithmeticException("NO PUEDES DIVIDIR POR CERO, chaval");
         return num1/num2;
     }
 
